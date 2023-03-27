e002d87bc1b84a0ef77f2c97b97829b20c516904
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-03-27 16:59:17 +0900

    Revert "fix ro PKG for Person"
    This reverts commit e916e26d22f5795e551e92a09a6f6c838116714c.

--- tests/ro_generator/test_gin_ro_generator.py
+++ tests/ro_generator/test_gin_ro_generator.py
@@ -1889,12 +1889,7 @@ class TestGinRoGenerator(TestCase):
 
         try:
             ro_crate = ro_gnt.generate()
-            #print(json.dumps(ro_crate, indent=4))
-            ro = json.dumps(ro_crate, indent=4)
-            write_file_name = './tests/results/'+'result_' + file_name
-            f = open(write_file_name, 'w')
-            f.write(ro)
-            f.close()
+            print(ro_crate)
         except JsonValidationError as e:
             error_dict = e.get_err_msg_for_check_key()
             for key in error_dict.keys():
